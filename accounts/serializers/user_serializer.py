0ade52c494068db0f9ef218778284335b7834ef7
Allan Katongole <kapsonkatongole@gmail.com>
2020-11-12 16:32:58 +0300

    Add otp aspects

--- accounts/serializers/user_serializer.py
+++ accounts/serializers/user_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import User
+from ..models import User, Otp
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,9 +10,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password', 'phone_number', 'location')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password', 'prefix', 'location')
 
-    
+    #validation at the serializer, This should be refactored later.
+    def validate_username(self, user):
+        if len(user) != 9:
+            raise serializers.ValidationError("username should be 9 characters")
+        return user
+
     def create(self, validated_data):
         user = super(UserSerializer, self).create(validated_data)
         user.set_password(validated_data['password'])
@@ -20,13 +25,24 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+
+class UserCreateSerializer(serializers.Serializer):
+    phone_prefix = serializers.CharField(max_length=4)
+    phone_number = serializers.CharField(max_length=9)
+    first_name   = serializers.CharField(max_length=250)
+    last_name    = serializers.CharField(max_length=250)
+    location     = serializers.CharField(max_length=250)
+    email        = serializers.CharField(max_length=250)
+    pin_code     = serializers.IntegerField()
+
+
 class UserSaveSerializer(serializers.ModelSerializer):
     """
     Save serializer for User details
     """
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'phone_number', 'location')
+        fields = ('id', 'first_name', 'last_name', 'prefix', 'username', 'email', 'location')
 
 class UserShortDetailsSerializer(serializers.ModelSerializer):
     """
@@ -34,10 +50,22 @@ class UserShortDetailsSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'location', 'phone_number')
+        fields = ('id', 'first_name', 'last_name', 'location', 'prefix', 'username')
 
 class TokenSerializer(serializers.Serializer):
     """
     This serializer serializes the token data
     """
     token = serializers.CharField(max_length=255)
+
+class OtpSerializer(serializers.ModelSerializer):
+    """
+    Save serializer for the OTP
+    """
+    class Meta:
+        model = Otp
+        fields = ('id', 'user' ,'otp_code')
+
+class OtpVerificationSerializer(serializers.Serializer):
+    phone_number    = serializers.CharField(max_length=9)
+    otp             = serializers.IntegerField()
--- accounts/views/user_view.py
+++ accounts/views/user_view.py
@@ -1,14 +1,15 @@
-from accounts.models import User
+from accounts.models import User, Otp
 from django.contrib.auth import authenticate, login
 from rest_framework_jwt.settings import api_settings
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..serializers.user_serializer import UserSerializer, TokenSerializer, UserSaveSerializer
+from ..serializers.user_serializer import UserSerializer, TokenSerializer, UserSaveSerializer, UserCreateSerializer, OtpVerificationSerializer
 from loyalty.serializers.loyalty_serializer import LoyaltyUserCreateSerializer, LoyaltyTenantsDetailsSerializer
 
 from loyalty.models import LoyaltyUserPoints, LoyaltyTenants
+from ..helpers.accounts import send_otp
 
 # Get the JWT settings, add these lines after the import/from lines
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -31,58 +32,103 @@ class LoginView(APIView):
 
         # try:
         user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:   
+            if user.active:
+                login(request, user)
+                token_serializer = TokenSerializer(data={
+                    # using drf jwt utility functions to generate a token
+                    "token": jwt_encode_handler(
+                        jwt_payload_handler(user)
+                    )})
+                token_serializer.is_valid()
+                user_serializer = UserSerializer(user)
 
-        if user is not None:
-            
-            login(request, user)
-            token_serializer = TokenSerializer(data={
-                # using drf jwt utility functions to generate a token
-                "token": jwt_encode_handler(
-                    jwt_payload_handler(user)
-                )})
-            token_serializer.is_valid()
-            user_serializer = UserSerializer(user)
+                try:
+                    if user_type == "0":
+                        login_data = {"user_data":user_serializer.data, "token_data":token_serializer.data}
+                        return Response(login_data)
 
-            try:
-                if user_type == "0":
-                    login_data = {"user_data":user_serializer.data, "token_data":token_serializer.data}
-                    return Response(login_data)
-
-                else:
-                    tenant = LoyaltyTenants.objects.get(related_user_account=user_serializer.data['id'])
-                    tenant_serializer = LoyaltyTenantsDetailsSerializer(tenant)
-                    login_data = {"user_data":user_serializer.data, "tenant_data":tenant_serializer.data, "token_data":token_serializer.data}
-                    return Response(login_data)
-            except:
-                return Response({"error": "Contact support and stop being silly"})
-
+                    else:
+                        tenant = LoyaltyTenants.objects.get(related_user_account=user_serializer.data['id'])
+                        tenant_serializer = LoyaltyTenantsDetailsSerializer(tenant)
+                        login_data = {"user_data":user_serializer.data, "tenant_data":tenant_serializer.data, "token_data":token_serializer.data}
+                        return Response(login_data)
+                except:
+                    return Response({"error": "Contact support and stop being silly"})
+            return Response({"status": 403, "error":"User not activated, Please contact support"})
         return Response({"error": "invalid username or password"}, status=status.HTTP_404_NOT_FOUND)
-        # except:
-        #     return Response({"error": "User doesnot exist"}, status=status.HTTP_404_NOT_FOUND)
+
 
 
 class UserListView(APIView):
     """
-    List all users and create a new user.
+    Create a new user.
     """
-    # permission_classes = (permissions.IsAuthenticated, )
-
-    # def get(self, request, format=None):
-    #     serializer = UserSerializer(User.objects.filter(active=True), many=True)
-    #     return Response({"status":200, "data":serializer.data}, status=status.HTTP_200_OK)
-
     def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
+        serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid():
-            
-            serializer.save()
+            try:
+                user_data = {
+                    "first_name":serializer.data["first_name"],
+                    "last_name":serializer.data["last_name"],
+                    "username":serializer.data["phone_number"],
+                    "email":serializer.data["email"],
+                    "password":serializer.data["pin_code"],
+                    "prefix":serializer.data["phone_prefix"],
+                    "location":serializer.data["location"]
+                }
 
-            #create loyalty account
-            # loyalty_account_creation = LoyaltyUserCreateSerializer(data={'related_user':serializer.data['id']})
-            # loyalty_account_creation.is_valid(raise_exception=True)
-            # loyalty_account_creation.save()
-            return Response({"status":201, "data":serializer.data}, status=status.HTTP_201_CREATED)
+        
+                user_data_serialized = UserSerializer(data=user_data) 
+                user_data_serialized.is_valid(raise_exception=True)
+                user_data_serialized.save()
+
+                phone_number = "{}{}".format(user_data_serialized.data["prefix"], user_data_serialized.data["username"])
+
+                send_otp(user_data_serialized.data['id'], phone_number, user_data_serialized.data["first_name"])
+                return Response({"status":201, "data":user_data_serialized.data}, status=status.HTTP_201_CREATED)
+            except:
+                return Response({"status":500, "error":"Something only I can explain happened"})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VerifyUserOtp(APIView):
+    """
+    Verify a user's otp and activate their account
+    """
+    def post(self, request, format=None):
+        serializer = OtpVerificationSerializer(data=request.data)
+        if serializer.is_valid():
+            phone_number = serializer.data["phone_number"]
+            user = User.objects.get(username=phone_number)
+           
+            otp_object = Otp.objects.get(user=user)
+
+            if not user.active:
+                if int(otp_object.otp_code) == serializer.data["otp"]:
+                    
+                    User.objects.update_or_create(
+                    id=user.id, defaults={'active':True}
+                    )
+
+                    token_serializer = TokenSerializer(data={
+                    # using drf jwt utility functions to generate a token
+                    "token": jwt_encode_handler(
+                        jwt_payload_handler(user)
+                    )})
+                    token_serializer.is_valid()
+                    user_serializer = UserSerializer(user)
+
+                    login_data = {"user_data":user_serializer.data, "token_data":token_serializer.data}
+
+                    return Response({"status":200, "data": login_data}, status=status.HTTP_200_OK)
+                
+                return Response({"status":400, "error":"Bad Otp code"})
+            return Response({"status":200, "message":"User already activated"}, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
