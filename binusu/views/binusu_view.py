2ade1727387b16cd57d894da21434239fdfaa099
Allan Katongole <kapsonkatongole@gmail.com>
2021-01-13 14:46:38 +0300

    Complete buy and sell emails

--- binusu/views/binusu_view.py
+++ binusu/views/binusu_view.py
@@ -8,7 +8,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from ..serializers.serializers import KycSerializer, KycConfirmSerializer, OrdersSerializer, EmailLogsSerializer, TelegramLogsSerializer, OrderReceiverSerializer, KycUserSerializer
 
 from ..helpers.helpers import get_random_alphanumeric_string
-from ..helpers.email_handler import send_order_email, buy_email
+from ..helpers.email_handler import send_order_email, buy_email, client_email, sell_email, client_sell_email
 
 class KycListView(APIView):
     """
@@ -53,29 +53,53 @@ class OrdersView(APIView):
     def post(self, request, format=None):
         serializer = OrderReceiverSerializer(data=request.data)
         if serializer.is_valid():
-            # try: 
-            user = Kyc.objects.get(id=int(serializer.data['related_kyc']))
-            order_data = {
-                "order_number": "{}{}".format("BN", get_random_alphanumeric_string(10)),
-                "related_kyc": serializer.data['related_kyc'],
-                "order_type": serializer.data['order_type'],
-                "crypto_type": serializer.data['crypto_type'],
-                "fiat_type":    serializer.data['fiat_type'],
-                "order_amount_crypto": serializer.data['order_amount_crypto'],
-                "order_amount_fiat": serializer.data['order_amount_fiat'],
-                "order_status": "UNFULFILLED",
-                "crypto_unit_price": serializer.data['crypto_unit_price']
-            }
+            try: 
+                user = Kyc.objects.get(id=int(serializer.data['related_kyc']))
+                
+                order_data = {
+                    "order_number": "{}{}".format("BN", get_random_alphanumeric_string(10)),
+                    "related_kyc": serializer.data['related_kyc'],
+                    "order_type": serializer.data['order_type'],
+                    "crypto_type": serializer.data['crypto_type'],
+                    "fiat_type":    serializer.data['fiat_type'],
+                    "order_amount_crypto": serializer.data['order_amount_crypto'],
+                    "order_amount_fiat": serializer.data['order_amount_fiat'],
+                    "order_status": "UNFULFILLED",
+                    "crypto_unit_price": serializer.data['crypto_unit_price']
+                }
 
-            order_serializer = OrdersSerializer(data=order_data)
-            order_serializer.is_valid(raise_exception=True)
-            order_serializer.save()
+                order_serializer = OrdersSerializer(data=order_data)
+                order_serializer.is_valid(raise_exception=True)
+                order_serializer.save()
 
-            message = buy_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
+                message = buy_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
 
-            send_order_email("Crypto Buy Order", message, "kapson65536@gmail.com")
+                client_message = client_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
 
-            return Response({"status":201, "data":order_serializer.data}, status=status.HTTP_201_CREATED)
-            # except:
-            #     return Response({"status":404, "error":"User doesnt have valid KYC"}, status=status.HTTP_201_CREATED)
+                if(order_serializer.data["order_type"]=="BUY"):
+                    message = buy_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
+
+                    client_message = client_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
+
+                    send_order_email("Crypto Buy Order", message, "brian.t@savannah.ug")
+
+                    send_order_email("Crypto Buy Order", message, "arinrony@gmail.com")
+
+                    send_order_email("Cryptocurreny Purchase order from Binusu", client_message, user.email_address)
+                
+                else:
+                    message = sell_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
+
+                    client_message = client_sell_email(order_serializer.data["order_number"], order_serializer.data["order_type"], order_serializer.data["crypto_type"], order_serializer.data["fiat_type"], order_serializer.data["order_amount_crypto"], order_serializer.data["order_amount_fiat"], order_serializer.data["crypto_unit_price"])
+
+                    send_order_email("Crypto Sell Order", message, "brian.t@savannah.ug")
+
+                    send_order_email("Crypto Sell Order", message, "arinrony@gmail.com")
+
+                    send_order_email("Cryptocurreny Sell order from Binusu", client_message, user.email_address)
+
+
+                return Response({"status":201, "data":order_serializer.data}, status=status.HTTP_201_CREATED)
+            except:
+                return Response({"status":404, "error":"User doesnt have valid KYC"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
