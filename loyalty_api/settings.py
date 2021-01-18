1e6f9d189d9dd2be6ef391880bd4cbfdda309543
Ubuntu <multichain@MC1.mwoyweg3widejczxkvhqxrlzif.jnbx.internal.cloudapp.net>
2021-01-18 15:40:54 +0300

    Changes

--- loyalty_api/settings.py
+++ loyalty_api/settings.py
@@ -184,5 +184,7 @@ AUTH_USER_MODEL = 'accounts.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kapsonkatongole@gmail.com'
+EMAIL_HOST_PASSWORD = 'pdtedbdslcnfozki'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
