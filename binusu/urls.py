e2cc596f901497af53d6fbb82e7cd75763c15baa
Allan Katongole <kapsonkatongole@gmail.com>
2021-05-10 23:40:57 +0300

    Add fees type to backend

--- binusu/urls.py
+++ binusu/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views.binusu_view import KycListView, OrdersView, ConfirmKyc, ResetPassword, ConfirmPasswordReset, GetCurrentRates, OrdersStatistics, UpdateOrderDetails
+from .views.binusu_view import KycListView, OrdersView, ConfirmKyc, ResetPassword, ConfirmPasswordReset, GetCurrentRates, OrdersStatistics, UpdateOrderDetails, ClientOrdersView
 from .views.account_view import UserListView, UserLoginView, UserConfirmAccount
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('binusu/verify_kyc', ConfirmKyc.as_view(), name="binusu-verify-kyc"),
     path('binusu/orders', OrdersView.as_view(), name="binusu-orders"),
     path('binusu/orders/stats', OrdersStatistics.as_view(), name="binusu-orders-stats"),
+    path('binusu/orders/clients', ClientOrdersView.as_view(), name="binusu-clients-orders"),
     path('binusu/orders/update', UpdateOrderDetails.as_view(), name="binusu-orders-update"),
     path('binusu/password_reset', ResetPassword.as_view(), name="binusu_password_reset"),
     path('binusu/confirm_reset', ConfirmPasswordReset.as_view(), name="binusu_confirm_reset"),
